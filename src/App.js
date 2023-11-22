3d60d905a052c649298c8b936107dc42be4a7ad7
Abdurahmon <abdurahmon2004nuriy@gmail.com>
2023-11-22 12:21:32 +0500

    update our menu-items to navigate with useNavigate hook)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = (props) => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="shop/hats" element={<HatsPage />} />
+      </Routes>
+      {/* exact accepts booleans, if there is no value then it's true by default*/}
     </div>
   );
 }
