c9e1f4ce25a983040d374d695037c3b29fc8047c
Abdurahmon <abdurahmon2004nuriy@gmail.com>
2023-11-25 19:02:35 +0500

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = (props) => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route exact path="shop/hats" element={<HatsPage />} />
+        <Route exact path="/shop" element={<ShopPage />} />
       </Routes>
       {/* exact accepts booleans, if there is no value then it's true by default*/}
     </div>
